84af91a4213131b4b618c314da2258f70bb7adc9
Aditya Rana <adityarana181105@gmail.com>
2025-07-13 23:15:09 +0530

    Changes Made

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ app.use(session({
 }));
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "ReadStack",
-  password: "181105",
-  port: 5432
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database:process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT
 })
 
 db.connect();
